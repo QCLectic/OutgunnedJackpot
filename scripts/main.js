2ee754f36ffa20abc3df6ac22514ab8e4a235fa4
QClectic <54535437+QCLectic@users.noreply.github.com>
2023-06-02 09:48:56 -0500

    got single die to roll, now need to use slider to make multiple die rolls, store them, then display them in the display area.

--- scripts/main.js
+++ scripts/main.js
@@ -5,4 +5,20 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
-} 
+};
+
+const getRandomNumber= (min,max)=> {
+    return Math.floor(Math.random() * (max - min + 1))+ min;
+};
+
+const rollDie= ()=> getRandomNumber(1,6);
+
+document.getElementById("rollDiceButton").addEventListener("click", function(){
+    const reel= rollDie();
+    let reelResult= reel;
+    document.getElementById("rollResult").innerHTML= '<p>'+ reelResult +'</p>';
+});
+
+function rollDiceButton(){
+    return 
+};
