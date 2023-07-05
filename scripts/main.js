6ea88e3c92508378a574d8f6855939c7c1e640e8
QClectic <54535437+QCLectic@users.noreply.github.com>
2023-07-05 16:17:55 -0500

    while testing the results. i noticed i didnt handle the greater than 6 on the maxCount. I also found that I could have used elseif with triple = signs.however the case # seems cleaner.

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 // display number of dice to roll, between 2 and 9
 let diceNumRoll = document.getElementById("diceNumRoll");
 let diceNum = document.getElementById("diceNum");
-
+let rolledDice;
 diceNumRoll.addEventListener("input", function () {
   let diceNumRollValue = diceNumRoll.value;
   diceNum.textContent = diceNumRollValue;
@@ -15,10 +15,10 @@ function rollDie() {
 
 let rollBtn = document.getElementById("rollBtn");
 
-function diceRoll() {
-  let diceNumRollValue = diceNumRoll.value;
+function diceRoll(numRolls) {
+  //let diceNumRollValue = diceNumRoll.value; in row below change numRolls from the value to left
   let dieResult = [];
-  for (let i = 0; diceNumRollValue > i; i++) {
+  for (let i = 0; numRolls > i; i++) {
     dieResult.push(rollDie());
   }
 
@@ -63,8 +63,6 @@ function outcomeFun(arr) {
   }
   console.log(maxCount);
   switch (maxCount) {
-    case 1:
-      return "Failure! Bummer :(";
     case 2:
       return "Basic Success. Care to roll again?";
     case 3:
@@ -73,18 +71,21 @@ function outcomeFun(arr) {
       return "Extreme Success!";
     case 5:
       return "Impossible Success!";
-    case 6:
-      return "Jackpot! You are driving the scene!";
     default:
-      break;
+      if (maxCount>5){
+        return "Jackpot! You are driving the scene!"
+      }else {
+        return "Failure! Bummer :("
+      }
   }
 }
 
 //need to grab biggest value of array then based on value print result.
 // scratch that. need to see if at least one count in the array is greater than 2, then declaere the result. otherwise declare failure.
 
+
 rollBtn.addEventListener("click", function () {
-  let rolledDice = diceRoll();
+  rolledDice = diceRoll(diceNumRoll.value);
   resultDisplay.textContent = rolledDice;
 
   let sortedDice = diceSort(rolledDice);
@@ -102,17 +103,64 @@ rollBtn.addEventListener("click", function () {
 });
 // option to reroll lesser matches if there is at least one match. otherwise declare failure.
 let reRollBtn = document.getElementById("reRollBtn");
-let allInBtn= document.getElementById("allInBtn");
+let allInBtn = document.getElementById("allInBtn");
+let max = [];
+let notMax = [];
+
 reRollBtn.addEventListener("click", function () {
-  allInBtn.disabled =false;
+  max = [];
+  notMax = [];
+
+  // Find the maximum count of occurrences
+  let counter = {};
+  for (let element of rolledDice.flat()) {
+    counter[element] = (counter[element] || 0) + 1;
+  }
+  let maxCount = Math.max(...Object.values(counter));
+
+  // Split rolledDice into max and notMax arrays
+  for (let element of rolledDice.flat()) {
+    if (counter[element] === maxCount) {
+      max.push(element);
+    } else {
+      notMax.push(element);
+    }
+  }
+
+  // Roll the notMax array using its count as diceNumRollValue
+  let diceNumRollValue = notMax.length;
+  let newNotMax = diceRoll(diceNumRollValue);
+
+  // Combine max and newNotMax arrays into reRolledDice
+  let reRolledDice = max.concat(newNotMax);
+
+  // Update the resultDisplay, sortedDiceEm, and outcome
+  resultDisplay.textContent = reRolledDice;
+  sortedDiceEm.textContent = diceSort(reRolledDice);
+  outcome.textContent = outcomeFun(reRolledDice);
+  //disable the reRollBtn after roll. commented out for now during testing
+  reRollBtn.disabled = true;
+  //this needs to be conditional if outcome is not failure
+  allInBtn.disabled = false;
+
 });
 // if reroll results in additional match, declare greater success, or lose one success match
 // option to go all in, declare failure if no new match
 // reset state or restart game
-let resetBtn= document.getElementById("resetBtn");
-resetBtn.addEventListener("click",function(){
-  allInBtn.disabled=true;
-  reRollBtn.disabled=true;
+let resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", function () {
+  //resets buttons to original state
+  allInBtn.disabled = true;
+  reRollBtn.disabled = true;
+  // remove text
+  resultDisplay.textContent = "";
+  sortedDiceEm.textContent = "";
+  outcome.textContent = "";
+  // add line break to keep same spacing
+  let br = document.createElement("br");
+  resultDisplay.appendChild(br.cloneNode());
+  sortedDiceEm.appendChild(br.cloneNode());
+  outcome.appendChild(br.cloneNode());
 });
 //
 //
